a5c97a6adc68170461f3a1bf875112e7b99eb82e
Dan Wahlin <dwahlin@xmlforasp.net>
2016-02-05 11:04:19 -0700

    Updated DataService (basic error handling for observables)

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from 'angular2/core';
 import { Http, Response } from 'angular2/http';
-import 'rxjs/add/operator/map';
+
+//Grab everything with import 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/catch';
+
 
 @Injectable()
 export class DataService {
+  
+    baseUrl: string = '/src';
 
     constructor(private http: Http) { }
     
     getCustomers() {
-        return this.http.get('/src/customers.json')
-                        .map((res: Response) => res.json());
+        return this.http.get(this.baseUrl + '/customers.json')
+                        .map((res: Response) => res.json())
+                        .catch(this.handleError);
     }
 
     getOrders(){
-      return this.http.get('/src/orders.json')
-                        .map((res: Response) => res.json());                  
+      return this.http.get(this.baseUrl + '/orders.json')
+                      .map((res: Response) => res.json())
+                      .catch(this.handleError);               
+    }
+    
+    handleError(error: any) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
     }
 
 }
