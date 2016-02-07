17574731f2ceb782d301cadd6805c2ac87bb34da
Dan Wahlin <dwahlin@xmlforasp.net>
2016-02-07 12:22:16 -0700

    Simplifying file names/folder structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from 'angular2/core';
 import { ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
-import { CustomersComponent } from '../customers/customers.component';
-import { OrdersComponent } from '../orders/orders.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @Component({ 
   selector: 'app-container',
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import { bind } from 'angular2/core';
 import { FORM_PROVIDERS } from "angular2/common";
 import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
-import { AppComponent } from './appContainer/appContainer.component';
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
