7e5cff2bd56c4080728af515608e400ed4cab584
Dan Wahlin <dwahlin@xmlforasp.net>
2016-04-28 23:03:02 -0700

    Update for some future changes.

---
+++ bundle.js
@@ -0,0 +1,16 @@
+var SystemBuilder = require('systemjs-builder');
+var argv = require('yargs').argv;
+var builder = new SystemBuilder();
+
+builder.loadConfig('./system.config.js')
+  .then(function(){
+	  var outputFile = argv.prod ? 'dist/bundle.min.js' : 'dist/bundle.js';
+	  return builder.buildStatic('app', outputFile, {
+		  minify: argv.prod,
+		  mangle: argv.prod,
+		  rollup: argv.prod
+	  });
+  })
+  .then(function(){
+	  console.log('bundle built successfully!');
+  });
