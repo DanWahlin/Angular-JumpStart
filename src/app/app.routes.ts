0b620fd55ce7e5b715b9c4ef41519de03aaacd84
Dan Wahlin <dwahlin@xmlforasp.net>
2016-07-26 17:27:31 -0700

    Update const name for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { CustomersRoutes } from './customers/customers.routes';
 import { CustomerRoutes } from './customer/customer.routes';
 
-export const App_Routes: RouterConfig = [
+export const APP_ROUTES: RouterConfig = [
   ...CustomersRoutes,
   ...CustomerRoutes,
   { path: '**', pathMatch:'full', redirectTo: '/customers' } //catch any unfound routes and redirect to home page
 ];
 
 export const APP_ROUTER_PROVIDERS = [
-  provideRouter(App_Routes)
+  provideRouter(APP_ROUTES)
 ];
