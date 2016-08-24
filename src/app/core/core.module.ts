8afdc1b6da7d6362ab2ecac15e6a784dd203f261
John Papa <john@johnpapa.net>
2016-08-23 21:43:20 -0400

    added exception thing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DataService } from './services/data.service';
@@ -11,4 +11,24 @@ import { TrackByService } from './services/trackby.service';
   exports: [CommonModule],
   providers: [DataService, Sorter, TrackByService] // these should be singleton
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }  
+}
+
+
+// john;s crappy idea is below
+import { BaseException } from '@angular/core';
+
+export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
+  if (parentModule) {
+    throw new ModuleImportGuardException(moduleName);
+  }
+}
+
+class ModuleImportGuardException extends BaseException {
+  constructor(name: string) {
+    super(`${name} has already been loaded. Import Core modules in the AppModule only.`);
+  }
+}
