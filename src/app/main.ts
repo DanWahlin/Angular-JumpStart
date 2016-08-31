06bfe26759783f70cd1822dabf67ef680fa1ecd0
Dan Wahlin <dwahlin@xmlforasp.net>
2016-08-30 20:58:57 -0700

    Minor spacing

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
+
+import { AppModule } from './app.module';
 
 //enableProdMode(); //Uncomment for production
 platformBrowserDynamic().bootstrapModule(AppModule);
