14ad439a216b088ff0a08a042574b89f8c0dbd67
Dan Wahlin <dwahlin@xmlforasp.net>
2016-09-10 11:28:09 -0700

    Added then/catch to bootstrap

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,4 +4,6 @@ import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
 
 //enableProdMode(); //Uncomment for production
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log('App bootstrapped'))
+  .catch(err => console.error(err));
