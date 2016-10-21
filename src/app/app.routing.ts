b1645b8a0c5b69101b2a5df35dac38e602756afe
Dan Wahlin <dwahlin@xmlforasp.net>
2016-10-21 14:46:41 -0700

    Fixed lazy loaded module name

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 
 const app_routes: Routes = [
-  { path: 'customers/:id', loadChildren: 'app/customer/customer.module'},
+  { path: 'customers/:id', loadChildren: 'app/customer/customer.module#CustomerModule'},
   { path: '**', pathMatch:'full', redirectTo: '/customers' } //catch any unfound routes and redirect to home page
 ];
 
