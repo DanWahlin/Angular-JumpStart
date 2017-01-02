532c2fd83d15a05d0c59fd22f4af2df7be54e914
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-01-02 16:54:43 -0700

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { ModalService, IModalContent } from './core/modal/modal.service';
-
 @Component({ 
   moduleId: module.id,
   selector: 'cm-app-component',
