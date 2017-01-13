b4b47886968c5805e29885180896c9ba94032379
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-01-12 21:21:11 -0700

    Remove animation due to paging weirdness

--- src/app/customers/customers-card.component.ts
+++ src/app/customers/customers-card.component.ts
@@ -10,18 +10,18 @@ import { TrackByService } from '../core/services/trackby.service';
   templateUrl: 'customers-card.component.html',
   styleUrls: [ 'customers-card.component.css' ],
   //Add [@flyInOut]="'in'" into template on card
-  animations: [
-    trigger('flyInOut', [
-      state('in', style({transform: 'translateX(0)', opacity: 1})),
-      transition('void => *', [
-        style({transform: 'translateX(25%)', opacity: 0}),
-        animate(300)
-      ]),
-      transition('* => void', [
-        animate(300, style({transform: 'translateX(-25%)', opacity: 1}))
-      ])
-    ])
-  ],
+  // animations: [
+  //   trigger('flyInOut', [
+  //     state('in', style({transform: 'translateX(0)', opacity: 1})),
+  //     transition('void => *', [
+  //       style({transform: 'translateX(25%)', opacity: 0}),
+  //       animate(300)
+  //     ]),
+  //     transition('* => void', [
+  //       animate(300, style({transform: 'translateX(-25%)', opacity: 1}))
+  //     ])
+  //   ])
+  // ],
   //When using OnPush detectors, then the framework will check an OnPush 
   //component when any of its input properties changes, when it fires 
   //an event, or when an observable fires an event ~ Victor Savkin (Angular Team)
