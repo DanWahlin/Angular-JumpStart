e5f75603e276a93336f2018c936574c63eaa84b8
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-03-26 10:36:09 -0700

    Added fix for ngtools/webpack AotPlugin bug on Windows.

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -6,6 +6,26 @@ const webpack = require('webpack'),
       path = require('path'),
       rootDir = path.resolve(__dirname, '..');
 
+var aotPlugin = new ngToolsWebpack.AotPlugin({
+    tsConfigPath: "./tsconfig.aot.json",
+    entryModule: path.resolve(__dirname, "./src/app/app.module#AppModule"),
+});
+//Temporary hack to fix issue with AOT on Windows
+//https://github.com/angular/angular-cli/issues/5329
+aotPlugin._compilerHost._resolve = function(path_to_resolve) {
+    path_1 = require("path");
+    path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
+    if (path_to_resolve[0] == '.') {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
+    }
+    else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
+        return path_to_resolve;
+    }
+    else {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
+    }
+};
+
 const env = process.env.NODE_ENV;
 
 module.exports = webpackMerge(commonConfig, {
@@ -26,10 +46,11 @@ module.exports = webpackMerge(commonConfig, {
   }, 
   plugins: [
     //Angular AOT pluging
-    new ngToolsWebpack.AotPlugin({
-        mainPath: "src/app/main.ts",
-        tsConfigPath: './tsconfig.aot.json'
-    }),
+    // new ngToolsWebpack.AotPlugin({
+    //     mainPath: "src/app/main.ts",
+    //     tsConfigPath: './tsconfig.aot.json'
+    // }),
+    aotPlugin,
     new webpack.LoaderOptionsPlugin({
         minimize: true,
         debug: false
