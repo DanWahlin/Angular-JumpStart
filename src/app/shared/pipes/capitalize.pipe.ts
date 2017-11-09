0955013caf08d18aacf08bd3646aa48123db4b4e
Mahmoud Mouneer <m0neer@yahoo.com>
2017-11-09 03:20:41 +0200

    A Fix: Using string functions like `charAt` safely (#54)
    - Currently, passing an integer to the pipe will cause a thrown exception, so I updated it to check if the value is a string.
    - Optimizing the code by removing the unneeded `if` condition and falling back to the value passed itself.

--- src/app/shared/pipes/capitalize.pipe.ts
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -4,10 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CapitalizePipe implements PipeTransform {
 
   transform(value: any) {
-	  if (value) {
-      	return value.charAt(0).toUpperCase() + value.slice(1);
-	  }
-	  return value;
+    return typeof value === 'string' && value.charAt(0).toUpperCase() + value.slice(1) || value;
   }
-
 }
