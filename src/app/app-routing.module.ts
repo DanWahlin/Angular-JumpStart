be17a4fe671b710481e8f780222a1f51953282e7
Dan Wahlin <dwahlin@xmlforasp.net>
2019-05-22 22:41:09 +0200

    Update versions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { PreloadModulesStrategy } from './core/strategies/preload-modules.strate
 
 const app_routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/customers' },
-  { path: 'customers/:id', loadChildren: 'app/customer/customer.module#CustomerModule' },
+  { path: 'customers/:id', data: { preload: true }, loadChildren: 'app/customer/customer.module#CustomerModule' },
   { path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule' },
-  { path: 'orders', loadChildren: 'app/orders/orders.module#OrdersModule' },
+  { path: 'orders', data: { preload: true }, loadChildren: 'app/orders/orders.module#OrdersModule' },
   { path: 'about', loadChildren: 'app/about/about.module#AboutModule' },
   { path: '**', pathMatch: 'full', redirectTo: '/customers' } // catch any unfound routes and redirect to home page
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadAllModules }) ],
-  exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadModulesStrategy }) ],
+  exports: [ RouterModule ],
+  providers: [PreloadModulesStrategy]
 })
 export class AppRoutingModule { }
