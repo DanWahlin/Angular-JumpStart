bf223e6cd4c8de098687d214a0a37da8389086ec
Dan Wahlin <danwahlin@iMac-Office.local>
2020-05-14 21:34:39 -0700

    Triggering build

--- api/Login/index.js
+++ api/Login/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
         headers: {
             'Content-Type': 'application/json'    
         },
-        // status: 200, /* Defaults to 200 */
+        // status: 200, /* Defaults to 200  */
         body: true
     };
 }
