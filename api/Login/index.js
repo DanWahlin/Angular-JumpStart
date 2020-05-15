2a4fbe9b40f4abd4b4f09912e314cca49ac9c661
Dan Wahlin <danwahlin@iMac-Office.local>
2020-05-14 21:22:25 -0700

    Add content type

--- api/Login/index.js
+++ api/Login/index.js
@@ -2,6 +2,9 @@ module.exports = async function (context, req) {
     const userLogin = req.body;
 
     context.res = {
+        headers: {
+            'Content-Type': 'application/json'    
+        },
         // status: 200, /* Defaults to 200 */
         body: true
     };
--- api/Logout/index.js
+++ api/Logout/index.js
@@ -1,6 +1,9 @@
 module.exports = async function (context, req) {
     
     context.res = {
+        headers: {
+            'Content-Type': 'application/json'    
+        },
         // status: 200, /* Defaults to 200 */
         body: true
     };
