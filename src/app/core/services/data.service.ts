1f5236951c37ef18b8bbf3e424c84a3838870b28
Dan Wahlin <danwahlin@iMac-Office.local>
2020-05-15 00:19:25 -0700

    Temporary removal of CatchAll Function. Was causing strange issues with routes so removing for now.

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -79,7 +79,7 @@ export class DataService {
     }
 
     getStates(): Observable<IState[]> {
-        return this.http.get<IState[]>(this.customersBaseUrl + '/api/states')
+        return this.http.get<IState[]>(this.baseUrl + '/api/states')
             .pipe(catchError(this.handleError));
     }
 
