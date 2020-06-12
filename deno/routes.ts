f67845e505632a5db17690c2cc6852e00d824a5b
Dan Wahlin <danwahlin@iMac-Office.local>
2020-06-12 13:44:10 -0700

    Fixed request.body issue

--- deno/routes.ts
+++ deno/routes.ts
@@ -40,8 +40,8 @@ router.get<{id: string}>('/api/customers/:id', (ctx) => {
     ctx.response.body = selectedCustomer;
 });
 
-router.post('/api/customers', (ctx) => {
-    let postedCustomer = ctx.request.body as any;
+router.post('/api/customers', async (ctx) => {
+    let postedCustomer = (await ctx.request.body()).value;
     let maxId = Math.max.apply(Math,customers.map((cust: any) => cust.id));
     postedCustomer.id = ++maxId;
     postedCustomer.gender = (postedCustomer.id % 2 === 0) ? 'female' : 'male';
@@ -49,8 +49,9 @@ router.post('/api/customers', (ctx) => {
     ctx.response.body = postedCustomer;
 });
 
-router.put<{id: string}>('/api/customers/:id', (ctx) => {
-    let putCustomer = ctx.request.body as any;
+router.put<{id: string}>('/api/customers/:id', async (ctx) => {
+    let putCustomer = (await ctx.request.body()).value;
+    console.log(putCustomer);
     let id = +ctx.params.id;
     let status = false;
 
@@ -96,8 +97,8 @@ router.get('/api/states', (ctx) => {
     ctx.response.body = states;
 });
 
-router.post('/api/auth/login', (ctx) => {
-    var userLogin = ctx.request.body;
+router.post('/api/auth/login', async (ctx) => {
+    var userLogin = (await ctx.request.body()).value;
     //Add "real" auth here. Simulating it by returning a simple boolean.
     ctx.response.body = true;
 });
