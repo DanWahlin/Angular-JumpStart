16b60c3c07787096cc6ce761de558f5f2f2b8dee
Dan Wahlin <danwahlin@iMac-Office.local>
2020-06-12 11:51:15 -0700

    Simplify oak route params

--- deno/loadfile.ts
+++ deno/loadfile.ts
@@ -1,4 +1,4 @@
-export function loadFile(file: string) {
+export default function loadFile(file: string) {
     const decoder = new TextDecoder('utf-8'); 
     return decoder.decode(Deno.readFileSync(file));
 }
--- deno/routes.ts
+++ deno/routes.ts
@@ -1,13 +1,13 @@
-import { Router, Response, Request } from 'https://deno.land/x/oak/mod.ts';
-import { loadFile } from './loadfile.ts';
+import { Router } from 'https://deno.land/x/oak/mod.ts';
+import loadFile from './loadfile.ts';
 
 const   customers   = JSON.parse(loadFile('../data/customers.json')),
         states      = JSON.parse(loadFile('../data/states.json')),
         router = new Router();
 
-router.get('/api/customers/page/:skip/:top', ({params,  response}: {params: {skip: string, top: string}, response: Response}) => {
-    const topVal = +params.top,
-          skipVal = +params.skip,
+router.get<{skip: string, top: string}>('/api/customers/page/:skip/:top', (ctx) => {
+    const topVal = +ctx.params.top,
+          skipVal = +ctx.params.skip,
           skip = (isNaN(skipVal)) ? 0 : skipVal;  
     let top = (isNaN(topVal)) ? 10 : skip + (topVal);
 
@@ -18,16 +18,16 @@ router.get('/api/customers/page/:skip/:top', ({params,  response}: {params: {ski
     console.log(`Skip: ${skip} Top: ${top}`);
 
     var pagedCustomers = customers.slice(skip, top);
-    response.headers.set('X-InlineCount', customers.length);
-    response.body = pagedCustomers;
+    ctx.response.headers.set('X-InlineCount', customers.length);
+    ctx.response.body = pagedCustomers;
 });
 
-router.get('/api/customers', ({response}) => {
-    response.body = customers;
+router.get('/api/customers', (ctx) => {
+    ctx.response.body = customers;
 });
 
-router.get('/api/customers/:id', ({params,  response}: {params: {id: string}, response: Response}) => {
-    let customerId = +params.id;
+router.get<{id: string}>('/api/customers/:id', (ctx) => {
+    let customerId = +ctx.params.id;
     let selectedCustomer = null;
     for (let customer of customers) {
         if (customer.id === customerId) {
@@ -37,21 +37,21 @@ router.get('/api/customers/:id', ({params,  response}: {params: {id: string}, re
            break;
         }
     }  
-    response.body = selectedCustomer;
+    ctx.response.body = selectedCustomer;
 });
 
-router.post('/api/customers', ({request, response}: {request: Request, response: Response}) => {
-    let postedCustomer = request.body as any;
+router.post('/api/customers', (ctx) => {
+    let postedCustomer = ctx.request.body as any;
     let maxId = Math.max.apply(Math,customers.map((cust: any) => cust.id));
     postedCustomer.id = ++maxId;
     postedCustomer.gender = (postedCustomer.id % 2 === 0) ? 'female' : 'male';
     customers.push(postedCustomer);
-    response.body = postedCustomer;
+    ctx.response.body = postedCustomer;
 });
 
-router.put('/api/customers/:id', ({params, request, response}: {params: {id: string}, request: Request, response: Response}) => {
-    let putCustomer = request.body as any;
-    let id = +params.id;
+router.put<{id: string}>('/api/customers/:id', (ctx) => {
+    let putCustomer = ctx.request.body as any;
+    let id = +ctx.params.id;
     let status = false;
 
     //Ensure state name is in sync with state abbreviation 
@@ -68,42 +68,42 @@ router.put('/api/customers/:id', ({params, request, response}: {params: {id: str
             break;
         }
     }
-    response.body = { status: status };
+    ctx.response.body = { status: status };
 });
 
-router.delete('/api/customers/:id', ({params,  response}: {params: {id: string}, response: Response}) => {
-    let customerId = +params.id;
+router.delete<{id: string}>('/api/customers/:id', (ctx) => {
+    let customerId = +ctx.params.id;
     for (let i=0,len=customers.length;i<len;i++) {
         if (customers[i].id === customerId) {
            customers.splice(i,1);
            break;
         }
     }  
-    response.body = { status: true };
+    ctx.response.body = { status: true };
 });
 
-router.get('/api/orders/:id', ({params,  response}: {params: {id: string}, response: Response}) => {
-    let customerId = +params.id;
+router.get<{id: string}>('/api/orders/:id', (ctx) => {
+    let customerId = +ctx.params.id;
     for (let cust of customers) {
         if (cust.customerId === customerId) {
-            return response.body = cust;
+            return ctx.response.body = cust;
         }
     }
-    response.body = [];
+    ctx.response.body = [];
 });
 
-router.get('/api/states', ({response}: {response: Response}) => {
-    response.body = states;
+router.get('/api/states', (ctx) => {
+    ctx.response.body = states;
 });
 
-router.post('/api/auth/login', ({request, response}: {request: Request, response: Response}) => {
-    var userLogin = request.body;
+router.post('/api/auth/login', (ctx) => {
+    var userLogin = ctx.request.body;
     //Add "real" auth here. Simulating it by returning a simple boolean.
-    response.body = true;
+    ctx.response.body = true;
 });
 
-router.post('/api/auth/logout', ({response}: {response: Response}) => {
-    response.body = true;
+router.post('/api/auth/logout', (ctx) => {
+    ctx.response.body = true;
 });
 
 // router.all('/(.*)', (ctx) => {
