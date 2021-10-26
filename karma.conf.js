8ba0d990c0fdc6ac2b60f3dc0ee86bdb3e1defcd
Prasad <varaprasadmk123@gmail.com>
2021-10-26 11:10:58 +0530

    deveddd

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-// I have the change
+// I have the change the developer hari anna
 module.exports = function (config) {
   config.set({
     basePath: '',
