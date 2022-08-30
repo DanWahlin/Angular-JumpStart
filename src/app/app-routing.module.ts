75a1073fbab56bc40286931de6cd3edee68cf0b9
Dan Wahlin <you@example.com>
2022-08-30 09:53:07 -0600

    Remove old comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,6 @@ const app_routes: Routes = [
   { path: 'orders', data: { preload: true }, loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: '**', pathMatch: 'full', redirectTo: '/customers' } // catch any unfound routes and redirect to home page
-
-  // NOTE: If you're using Angular 7 or lower you'll lazy loads routes the following way
-  
-  // { path: 'customers/:id', data: { preload: true }, loadChildren: 'app/customer/customer.module#CustomerModule' },
-  // { path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule' },
-  // { path: 'orders', data: { preload: true }, loadChildren: 'app/orders/orders.module#OrdersModule' },
-  // { path: 'about', loadChildren: 'app/about/about.module#AboutModule' },
-
 ];
 
 @NgModule({
