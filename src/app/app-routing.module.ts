793767cd1b6da0ee3901d3ca372daccf44897421
danwahlin <dwahlin@xmlforasp.net>
2023-05-20 15:34:18 -0700

    Update to 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const app_routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadModulesStrategy, relativeLinkResolution: 'legacy' }) ],
+  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadModulesStrategy }) ],
   exports: [ RouterModule ],
   providers: [PreloadModulesStrategy]
 })
